9deb9be3c0e3d8c5c50678ac8b94fe32defbb74a
Petrol <35155443+Chizz3x@users.noreply.github.com>
2023-10-21 22:25:11 +0300

    routing fix

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
+export const ROUTES_PREFIX = "/verslas.github.io";
+
 export const ROUTES = {
-  home: "/",
-  ending: "/ending_page",
-  question: "/question/:seed/:page"
+  home: `${ROUTES_PREFIX}/`,
+  ending: `${ROUTES_PREFIX}/ending_page`,
+  question: `${ROUTES_PREFIX}/question/:seed/:page`
 };
