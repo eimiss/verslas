2d287a9e69a7cd1972b010a063b68afd219d853a
eimiss <83466779+eimiss@users.noreply.github.com>
2023-12-11 21:48:42 +0200

    added rules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import { ROUTES } from "../routes";
 import PageHome from "./Home";
 import EndingPage from "./ending_page";
 import PageQuestion from "./question/[seed]/[page]";
+import PageRules from "./rules";
 import Page404 from "./404";
 
 const PAGES: RouteProps[] = [
@@ -30,6 +31,12 @@ const PAGES: RouteProps[] = [
     path: ROUTES.question,
     element: <Layout>
       <PageQuestion />
+    </Layout>
+  },
+  {
+    path: ROUTES.rules,
+    element: <Layout>
+      <PageRules />
     </Layout>
   }
 ];
--- src/routes.ts
+++ src/routes.ts
@@ -3,5 +3,6 @@ export const ROUTES_PREFIX = "/verslas.github.io";
 export const ROUTES = {
   home: `${ROUTES_PREFIX}/`,
   ending: `${ROUTES_PREFIX}/ending_page`,
-  question: `${ROUTES_PREFIX}/question/:seed/:page`
+  question: `${ROUTES_PREFIX}/question/:seed/:page`,
+  rules: `${ROUTES_PREFIX}/rules`
 };
