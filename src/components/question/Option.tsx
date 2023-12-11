ea009ca75978348135d0bbc8fd8084fd4300c9ca
eimiss <83466779+eimiss@users.noreply.github.com>
2023-12-11 23:26:12 +0200

    updated

--- src/components/question/Option.tsx
+++ src/components/question/Option.tsx
@@ -2,25 +2,60 @@ import styled from "styled-components";
 import React, { useState } from "react";
 
 export type Option = {
-    title: string;
-    score: number;
-    explain?: string;
-}
+  title: string;
+  score: number;
+  explain?: string;
+};
 
 export type QuestionProps = {
-    maxScore: number;
-    option: Option;
-    index: number;
-}
+  maxScore: number;
+  option: Option;
+  index: number;
+};
 
-const QuestionOption = ({ option,maxScore, index }: QuestionProps) => {
-  const [ open, setOpen ] = useState<boolean>();
-const {title,score,explain} = option;
+const OptionWrapper = styled.div`
+  margin-bottom: 8px;
+`;
+
+const OptionText = styled.div<{ hasExplanation: boolean }>`
+  font-size: 1rem;
+  ${(props) => props.hasExplanation && "cursor: pointer; user-select: none;"}
+`;
+
+const ScoreText = styled.span<{ score: number; maxScore: number }>`
+  color: ${(props) =>
+    props.score === 0
+      ? "#e53e3e" // Replace with your red-500 color
+      : props.score === props.maxScore
+      ? "#38a169" // Replace with your green-500 color
+      : "#ed8936"}; // Replace with your orange-500 color
+`;
+
+const ExplanationText = styled.div<{ isOpen: boolean }>`
+  display: ${(props) => (props.isOpen ? "block" : "none")};
+  font-size: 0.875rem;
+  margin-top: 4px;
+  padding: 8px;
+  border-radius: 4px;
+  box-shadow: inset 0 -2px 2px 0px rgba(0, 0, 0, 0.2);
+`;
+
+const QuestionOption = ({ option, maxScore, index }: QuestionProps) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const { title, score, explain } = option;
+
   return (
-    <div>
-    <div className={`text-md ${explain ? 'cursor-pointer select-none' : ''}`} onClick={()=>setOpen((prev)=>!prev)}>{`${index + 1}. ${title} `}<span className={score == 0 ? "text-red-500" : score === maxScore ? "text-green-500" : "text-orange-500"}>{`(${score})`}</span></div>
-    { explain && <div className={!open ? "hidden" : "text-sm pl-4 px-2 py-2 mb-2 rounded-md"} style={{boxShadow:"inset 0 -2px 2px 0px rgba(0,0,0, 0.2)"}}>{explain}</div>}
-</div>
+    <OptionWrapper>
+      <OptionText hasExplanation={!!explain} onClick={() => setOpen((prev) => !prev)}>
+        {`${index + 1}. ${title} `}
+        <ScoreText score={score} maxScore={maxScore}>{`(${score})`}</ScoreText>
+      </OptionText>
+      {explain && (
+        <ExplanationText isOpen={open}>
+          {explain}
+        </ExplanationText>
+      )}
+    </OptionWrapper>
   );
 };
 
--- src/pages/questions/QuestionsScreen.tsx
+++ src/pages/questions/QuestionsScreen.tsx
@@ -1,31 +1,69 @@
-/* eslint-disable react/jsx-key */
 import React from "react";
+import styled from "styled-components";
 import { QUESTIONS } from "../../const";
 
-import { QuestionOption } from "../../components/question/Option";
+import {QuestionOption} from "../../components/question/Option";
 
 const QuestionsScreen = () => {
   return (
-    <div className="bg-slate-300">
-      {QUESTIONS.map((q, index) => {
-        return (
-          <div className="w-3/6 mx-auto " style={{ minWidth: 320 }}> {q.type && <div className="float-right ml-1 mb-1 p-4 border-2 border-slate-400 rounded-bl-md rounded-tr-md">{`${q.type}`}</div>}
-            <div className="my-4 p-4 rounded-md border-2 border-slate-400 shadow-inner shadow-slate-400" >
-              <div className="font-medium text-xl mb-2">{`${index + 1}. ${q.question}`}</div>
-              <div className="pl-4">
-                {q.options.map((o, optionIndex) => {
-                  return (
-                    <QuestionOption option={o} maxScore={Math.max(...q.options.map(option => option.score))} index={optionIndex}/>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <Container>
+      {QUESTIONS.map((q, index) => (
+        <QuestionContainer key={index}>
+          {q.type && <TypeContainer>{q.type}</TypeContainer>}
+          <QuestionWrapper>
+            <QuestionText>{`${index + 1}. ${q.question}`}</QuestionText>
+            <OptionsContainer>
+              {q.options.map((o, optionIndex) => (
+                <QuestionOption
+                  key={optionIndex}
+                  option={o}
+                  maxScore={Math.max(...q.options.map(option => option.score))}
+                  index={optionIndex}
+                />
+              ))}
+            </OptionsContainer>
+          </QuestionWrapper>
+        </QuestionContainer>
+      ))}
+    </Container>
   );
 };
 
-
 export default QuestionsScreen;
+
+const Container = styled.div`
+  background-color: #6b7280; /* Replace with your slate-300 color */
+`;
+
+const QuestionContainer = styled.div`
+  width: 50%; /* Adjust as needed */
+  margin: 0 auto;
+  min-width: 320px;
+`;
+
+const TypeContainer = styled.div`
+  float: right;
+  margin-left: 1px;
+  margin-bottom: 1px;
+  padding: 16px;
+  border: 2px solid #6b7280; /* Replace with your slate-400 color */
+  border-radius: 8px 0 0 8px;
+`;
+
+const QuestionWrapper = styled.div`
+  margin: 16px 0;
+  padding: 16px;
+  border: 2px solid #6b7280; /* Replace with your slate-400 color */
+  border-radius: 8px;
+  box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.1), 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
+const QuestionText = styled.div`
+  font-weight: bold;
+  font-size: 1.5rem;
+  margin-bottom: 8px;
+`;
+
+const OptionsContainer = styled.div`
+  padding-left: 16px;
+`;
